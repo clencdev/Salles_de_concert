ffde2b365b18ea32d9bb5860b01f010afa68d065
clencdev <clency.lj@gmail.com>
2023-11-09 23:41:36 +0100

    creation de la page nos Actus qui regroupe toutes les actus

---
+++ nosActu.php
@@ -0,0 +1,30 @@
+<?php
+$pageTitle = "Nos actus";
+require_once "layout/header.php";
+require_once "classes/ConnectionDb.php";
+try {
+
+
+    $pdo = ConnectionDb::getConnex();
+
+
+    $stmt = $pdo->query("SELECT * FROM actu");
+    $actus= $stmt->fetchAll();
+}catch(PDOException $e) {
+    echo 'failed' . $e->getMessage();
+}
+?>
+
+<body>
+    <main>
+    <h2 class=" text-center text-xl font-semibold text-blue-600/100 dark:text-blue-500/100">Actu</h2>
+        <div class="row">
+            <?php 
+            foreach($actus as $actu) 
+            {
+                require "templates/actu_template.php";
+            };
+            ?>
+        </div>
+    </main>
+</body>
