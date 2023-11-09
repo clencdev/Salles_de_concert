b577521e07cf21f068afdf92e029dafd9a6af876
clencdev <clency.lj@gmail.com>
2023-11-09 12:25:52 +0100

    creation de la pgae individuelle blog terminée

---
+++ actu.php
@@ -0,0 +1,29 @@
+<?php
+$titre="Actualité";
+require_once "layout/header.php";
+require_once"classes/ConnectionDb.php";
+
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    http_response_code(404);
+    echo 'actu non trouvé';
+    exit;  
+}
+
+
+['id' => $id ] = $_GET;
+try {
+
+
+    $pdo = ConnectionDb::getConnex();
+
+
+    $stmt = $pdo->prepare("SELECT * FROM actu WHERE actu_id = ?");
+    $stmt->execute([$id]);
+    $actu= $stmt->fetch();
+}catch(PDOException $e) {
+    echo 'failed' . $e->getMessage();
+}
+var_dump($actu);
+
+
+require_once 'templates/blog_template.php';
--- templates/blog_template.php
+++ templates/blog_template.php
@@ -8,15 +8,14 @@ require_once __DIR__ . '/../layout/header.php';
 <div class="container mx-auto p-4">
     <h1 class="text-4xl mb-4">Blog</h1>
 
-//article de base donnée
     <div class="bg-white p-4 rounded mb-4 shadow">
-        <img src="#" alt="Article Image" class="w-full h-56 object-cover rounded-t">
+        <img src="photo/photoactu/<?php echo $actu['images'] ?>" alt="Article Image"  class="mb-4 w-full h-73 object-cover rounded-t">
         <div class="p-4">
-            <h2 class="text-xl font-bold mb-2">Title from Database</h2>
-            <p class="text-gray-600">Published on: Date from Database</p>
+            <h2 class="text-xl font-bold mb-20"><?php echo $actu['title'] ?></h2>
+            <p class="text-gray-600 indent-8"><?php echo nl2br($actu['text_content']) ?></p>
         </div>
     </div>
-    <!-- Fin de la répétition -->
+    
 
 </div>
 
